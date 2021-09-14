4f609a496320a634bd6ac8152b7a4aa627f211f1
Karine MOREAU <kmoreau@cstj.qc.ca>
2021-09-14 16:08:52 -0400

    Fin du cours du 2021-09-14

---
+++ ProjetEnCours/Labo04-3While.cpp
@@ -0,0 +1,91 @@
+// But : Le programme demande un nombre de départ et un nombre de fin et 
+// le programme affiche les nombres compris entre le nombre de départ et le nombre de fin
+// nombre de départ : 5
+// nombre de fin : 10
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
+// 
+// nombre de départ : 10
+// nombre de fin : 5
+// 10
+// 9
+// 8
+// 7
+// 6
+// 5
+// 
+// Auteur : Karine Moreau
+// Date : 2021-09-14
+
+#include <iostream>
+
+using namespace std;
+
+
+void main()
+{
+   // Pour afficher correctement les accents
+   setlocale(LC_ALL, "");     
+
+   // Déclaration des constantes et initialisation (donner une valeur)
+ 
+   // Déclaration des variables
+   
+   int depart;
+   int fin;
+
+   // il y a des variables pour mémoriser les informations fournies par l'utilisateur.
+   // C'est la responsabilité de l'utilisateur de donner la première valeur à la note
+   cout << "Veuillez entrer le nombre de départ" << endl;
+   cin >> depart;
+
+   // il y a des variables pour calculer les résultats.
+   // C'est la responsabilité du programmeur d'y mettre la valeur de départ
+   cout << "Entrer le nombre de fin";
+   cin >> fin ;
+   
+   cout << "Avec la boucle for \n";
+ 
+   for (int compteur = depart; compteur <= fin; compteur++)
+   {
+      cout << compteur << endl;
+
+   }
+
+   cout << "Avec la boucle while \n";
+
+   int compteur = depart;
+   while (compteur <= fin)
+   {
+      cout << compteur << endl;
+
+      compteur++;
+   }
+
+
+
+
+   for (int compteur = depart; compteur >= fin; compteur--)
+   {
+      cout << compteur << endl;
+
+   }
+
+
+
+   system("pause");
+
+}
+// plan de test
+//  
+
+
+
+
+
+
+
---
+++ ProjetEnCours/Labo04-4While.cpp
@@ -0,0 +1,52 @@
+// But : Le programme demande à l'utilisateur une note qui doit être comprise entre 0 et 100.
+// Le programme affiche un message d'erreur tant que la note n'est pas comprise entre 0 et 100.
+// A la fin il affiche la note correcte
+// Auteur : Karine Moreau
+// Date : 2021-09-14
+
+#include <iostream>
+
+using namespace std;
+
+
+void main()
+{
+   // Pour afficher correctement les accents
+   setlocale(LC_ALL, "");     
+
+   // Déclaration des constantes et initialisation (donner une valeur)
+   const double NOTE_MIN = 0;
+   const double NOTE_MAX = 100;
+
+   // Déclaration des variables
+   // il y a des variables pour mémoriser les informations fournies par l'utilisateur.
+   // C'est la responsabilité de l'utilisateur de donner la première valeur à la note
+   double note;
+
+   // TODO : demander la note à l'utilisateur : elle doit être comprise entre 0 et 100 : 
+
+
+   // TODO : Tant que la note n'est pas comprise entre 0 et 100, insulter l'utilisateur
+
+
+
+   // TODO : Le programme affiche la note qui doit être comprise entre 0 et 100
+
+   
+
+
+
+
+
+   system("pause");
+
+}
+// plan de test
+//  
+
+
+
+
+
+
+
