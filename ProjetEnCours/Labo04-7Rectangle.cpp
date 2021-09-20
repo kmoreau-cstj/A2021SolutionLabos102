be77ae98f67e2bbcd2683156457eb6c5abd9720d
Karine MOREAU <kmoreau@cstj.qc.ca>
2021-09-20 16:59:07 -0400

    Fin du cours du 2021-09-20

---
+++ ProjetEnCours/Labo04-7Rectangle.cpp
@@ -0,0 +1,52 @@
+// But : Demander à l'utilisateur deux nombres : un pour la longueur et l'autre pour la hauteur
+// Le programme dessine un rectangle de longueur par hauteur
+// 
+// Par exemple : longueur = 4 hauteur = 3
+// ****
+// ****
+// ****
+// 
+// Auteur : Karine Moreau
+// Date : 2021-09-20
+
+
+
+
+#include <iostream>
+
+
+using namespace std;
+
+
+void main()
+{
+  // Pour afficher correctement les accents
+   setlocale(LC_ALL, "");     
+
+   // Déclaration des variables
+   int longueur;
+   int hauteur;
+
+   cout << "Veuillez entrer la longueur : ";
+   cin >> longueur;
+   cout << "Veuillez entrer la hauteur : ";
+   cin >> hauteur;
+
+
+   // TODO : dessiner les étoiles pour faire la forme rectangulaire avec deux boucles for
+
+ 
+  
+
+   system("pause");
+
+}
+// plan de test
+//  
+
+
+
+
+
+
+
