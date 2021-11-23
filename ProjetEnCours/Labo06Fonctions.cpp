ec31935db6bdd2f886bd1846847f309175e8c40f
Karine Moreau <70488225+kmoreau-cstj@users.noreply.github.com>
2021-11-23 15:20:56 -0500

    Update Labo06Fonctions.cpp
    Correction du chaine vide en date du 2021-11-23

--- ProjetEnCours/Labo06Fonctions.cpp
+++ ProjetEnCours/Labo06Fonctions.cpp
@@ -4,11 +4,11 @@
 bool etreUnNombre(string clavier)
 {
    // Est-ce que clavier commence par un chiffre
-   if (clavier.at(0) >= '0' && clavier[0] <= '9')
+   if (!clavier.empty() && clavier.at(0) >= '0' && clavier[0] <= '9')
    {
       return true;
    }
-   else if (clavier.front() == '-' && clavier.at(1) >= '0' && clavier[1] <='9')
+   else if (clavier.size() >= 2 && clavier.front() == '-' && clavier.at(1) >= '0' && clavier[1] <='9')
    {
       return true;
    }
